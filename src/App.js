9d54605da2ce870c45cca51b7cd527b6f6041256
Gabriel Logan <logandeveloper1000@gmail.com>
2022-10-26 01:16:12 -0400

    Created Pages: Sign In, Sign Up, Forgot Password and the component: OAuth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
-import SignOut from "./pages/SignOut";
+import SignUp from "./pages/SignUp";     
 import Header from './components/Header';
 
 function App() {
@@ -18,7 +17,7 @@ function App() {
         <Route path='/offers' element={<Offers />} />
         <Route path='/profile' element={<Profile />} />
         <Route path='/Sign-in' element={<SignIn />} />
-        <Route path='/Sign-out' element={<SignOut />} />
+        <Route path='/Sign-up' element={<SignUp />} />
       </Routes>
     </Router>
   );
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,13 +3,14 @@ import {AiFillEyeInvisible,AiFillEye} from 'react-icons/ai'
 import { Link, useNavigate } from "react-router-dom";
 import OAuth from '../components/OAuth';
 
-export default function SignIn() {
+export default function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
+    home: "",
     email: "",
     password: ""
   });
-  const {email, password} = formData;
+  const {name, email, password} = formData;
   function onChange(e) {
     setFormData((previous) => ({
       ...previous,
@@ -18,13 +19,21 @@ export default function SignIn() {
   }
   return (
     <section>
-      <h1 className='text-3xl text-center mt-6 font-bold'>Sign In</h1>
+      <h1 className='text-3xl text-center mt-6 font-bold'>Sign Up</h1>
       <div className='flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto'>
         <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
           <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80" alt="key" lassName="w-full rounded-2xl"/>
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
           <form>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={onChange}
+              placeholder="Full Name"
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+            />
             <input
               type="email"
               id="email"
@@ -58,10 +67,10 @@ export default function SignIn() {
               <p className="mb-6">
                 Don't have a account?
                 <Link
-                  to="/sign-up"
+                  to="/sign-in"
                   className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1"
                 >
-                  Register
+                  Sign In
                 </Link>
               </p>
               <p>
@@ -77,7 +86,7 @@ export default function SignIn() {
               className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
               type="submit"
             >
-              Sign in
+              Sign Up
             </button>
             <div className="flex items-center  my-4 before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
               <p className="text-center font-semibold mx-4">OR</p>
