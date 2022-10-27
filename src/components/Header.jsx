404f24e97e10e7e7f070ef0b79db0f27b945c432
Gabriel Logan <logandeveloper1000@gmail.com>
2022-10-27 15:02:40 -0400

    SignIn functions completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,6 @@ import {useLocation, useNavigate} from "react-router-dom"
 export default function Header() {
   const location = useLocation();
   const navigate = useNavigate();
-  console.log(location.pathname)
   function pathMatchRoute(route){
     if(route === location.pathname){
       return true
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,6 +2,8 @@ import {React, useState} from 'react'
 import {AiFillEyeInvisible,AiFillEye} from 'react-icons/ai'
 import { Link, useNavigate } from "react-router-dom";
 import OAuth from '../components/OAuth';
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+import { toast } from "react-toastify";
 
 export default function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
@@ -10,11 +12,25 @@ export default function SignIn() {
     password: ""
   });
   const {email, password} = formData;
+  const navigate = useNavigate()
   function onChange(e) {
     setFormData((previous) => ({
       ...previous,
       [e.target.id]: e.target.value
     }))
+  }
+  async function onSubmit(e){
+    e.preventDefault()
+    try{
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(auth, email, password)
+
+      if(userCredential.user){
+        navigate("/")
+      }
+    }catch (error){
+
+    }
   }
   return (
     <section>
@@ -24,7 +40,7 @@ export default function SignIn() {
           <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80" alt="key" className="w-full rounded-2xl"/>
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               type="email"
               id="email"
